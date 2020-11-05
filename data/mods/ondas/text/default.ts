72b3e2178d6590ecdfef2401513a1448973d4e42
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2020-11-05 22:55:14 +0000

    Create default.ts

---
+++ data/mods/ondas/text/default.ts
@@ -0,0 +1,58 @@
+export const DefaultText = {
+  
+	blst: {
+		start: "  [POKEMON] was blistered! Its physical moves will do slightly less damage!",
+		alreadyStarted: "#brn",
+		end: "#brn",
+		endFromItem: "#brn",
+		damage: "#brn",
+	},
+	frz: {
+		start: "  [POKEMON] was frozen solid! It's surrounded by a block of ice!",
+		alreadyStarted: "  [POKEMON] is already frozen solid!",
+		end: "  [POKEMON] thawed out!",
+		endFromItem: "  [POKEMON]'s [ITEM] defrosted it!",
+		endFromMove: "  [POKEMON]'s [MOVE] melted the ice!",
+		cant: "[POKEMON] is frozen solid!",
+		block: "  [POKEMON] couldn't be hit through the ice!",
+	},
+	shk: {
+		start: "  [POKEMON] was shocked! Its Speed will be lowered slightly until its next move!",
+		alreadyStarted: "  [POKEMON] is already shocked!",
+		end: "  [POKEMON] was cured of its shock!",
+		endFromItem: "  [POKEMON]'s [ITEM] cured its shock!",
+	},
+	par: {
+		start: "  [POKEMON] was paralyzed! Its Speed will be lowered, and its moves will consume more PP!",
+		alreadyStarted: "  [POKEMON] is already paralyzed!",
+		end: "  [POKEMON] was cured of paralysis!",
+		endFromItem: "  [POKEMON]'s [ITEM] cured its paralysis!",
+	},
+	bane: {
+		start: "  [POKEMON] was critically poisoned!",
+		startFromItem: "  [POKEMON] was critically poisoned by the [ITEM]!",
+		end: "#psn",
+		endFromItem: "#psn",
+		alreadyStarted: "#psn",
+		damage: "#psn",
+	},
+
+	flinch: {
+		cant: "[POKEMON] can't move from the shock!",
+	},
+
+
+	reversalroom: {
+		start: "  It created a bizarre area in which stat modifiers are swapped!",
+		end: "  Reversal Room wore off, and stat modifiers returned to normal!",
+	},
+
+	sandstorm: {
+		weatherName: "Sandstorm",
+		start: "  A sandstorm kicked up!",
+		end: "  The sandstorm subsided.",
+		upkeep: "  (The sandstorm is raging.)",
+		damage: "  [POKEMON] is buffeted by the sandstorm!",
+		blockMove: "  The powder couldn't reach its target!",
+	},
+};
