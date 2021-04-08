8459a916d57fbc52898c3964a9b8d8b0930b2b38
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-04-08 01:04:00 -0600

    Fusion Evolution UU: Move modifyDamage function

--- data/mods/feuu/scripts.ts
+++ data/mods/feuu/scripts.ts
@@ -99,63 +99,6 @@ export const Scripts: ModdedBattleScriptsData = {
 			}
 			return item.megaStone;
 		},
-	},
-
-	pokemon: {
-		// Included for Magnetic Waves:
-		// Levitate is checked for when running groundedness (ground immunity, iron ball, etc)
-		// So we manually add a check for Magnetic Waves here as well,
-		// Including a diffrent activation message
-		// so that the game doesn't report it as having Levitate when it procs.
-		runImmunity(type: string, message?: string | boolean) {
-			if (!type || type === '???') return true;
-			if (!(type in this.battle.dex.data.TypeChart)) {
-				if (type === 'Fairy' || type === 'Dark' || type === 'Steel') return true;
-				throw new Error("Use runStatusImmunity for " + type);
-			}
-			if (this.fainted) return false;
-
-			const negateResult = this.battle.runEvent('NegateImmunity', this, type);
-			let isGrounded;
-			if (type === 'Ground') {
-				isGrounded = this.isGrounded(!negateResult);
-				if (isGrounded === null) {
-					if (message) {
-						if (this.hasAbility('magneticwaves')) {
-							this.battle.add('-immune', this, '[from] ability: Magnetic Waves');
-						} else {
-							this.battle.add('-immune', this, '[from] ability: Levitate');
-						}
-					}
-					return false;
-				}
-			}
-			if (!negateResult) return true;
-			if ((isGrounded === undefined && !this.battle.dex.getImmunity(type, this)) || isGrounded === false) {
-				if (message) {
-					this.battle.add('-immune', this);
-				}
-				return false;
-			}
-			return true;
-		},
-
-		isGrounded(negateImmunity = false) {
-			if ('gravity' in this.battle.field.pseudoWeather) return true;
-			if ('ingrain' in this.volatiles && this.battle.gen >= 4) return true;
-			if ('smackdown' in this.volatiles) return true;
-			const item = (this.ignoringItem() ? '' : this.item);
-			if (item === 'ironball') return true;
-			// If a Fire/Flying type uses Burn Up and Roost, it becomes ???/Flying-type, but it's still grounded.
-			if (!negateImmunity && this.hasType('Flying') && !('roost' in this.volatiles)) return false;
-			if (
-				(this.hasAbility('levitate') || this.hasAbility('magneticwaves')) &&
-				!this.battle.suppressingAttackEvents()
-			) return null;
-			if ('magnetrise' in this.volatiles) return false;
-			if ('telekinesis' in this.volatiles) return false;
-			return item !== 'airballoon';
-		},
 
 		// Included for Therapeutic:
 		// Burn status' Atk reduction and Guts users' immunity to it is hard-coded in battle.ts,
@@ -242,6 +185,63 @@ export const Scripts: ModdedBattleScriptsData = {
 
 			// ...but 16-bit truncation happens even later, and can truncate to 0
 			return tr(baseDamage, 16);
+		},
+	},
+
+	pokemon: {
+		// Included for Magnetic Waves:
+		// Levitate is checked for when running groundedness (ground immunity, iron ball, etc)
+		// So we manually add a check for Magnetic Waves here as well,
+		// Including a diffrent activation message
+		// so that the game doesn't report it as having Levitate when it procs.
+		runImmunity(type: string, message?: string | boolean) {
+			if (!type || type === '???') return true;
+			if (!(type in this.battle.dex.data.TypeChart)) {
+				if (type === 'Fairy' || type === 'Dark' || type === 'Steel') return true;
+				throw new Error("Use runStatusImmunity for " + type);
+			}
+			if (this.fainted) return false;
+
+			const negateResult = this.battle.runEvent('NegateImmunity', this, type);
+			let isGrounded;
+			if (type === 'Ground') {
+				isGrounded = this.isGrounded(!negateResult);
+				if (isGrounded === null) {
+					if (message) {
+						if (this.hasAbility('magneticwaves')) {
+							this.battle.add('-immune', this, '[from] ability: Magnetic Waves');
+						} else {
+							this.battle.add('-immune', this, '[from] ability: Levitate');
+						}
+					}
+					return false;
+				}
+			}
+			if (!negateResult) return true;
+			if ((isGrounded === undefined && !this.battle.dex.getImmunity(type, this)) || isGrounded === false) {
+				if (message) {
+					this.battle.add('-immune', this);
+				}
+				return false;
+			}
+			return true;
+		},
+
+		isGrounded(negateImmunity = false) {
+			if ('gravity' in this.battle.field.pseudoWeather) return true;
+			if ('ingrain' in this.volatiles && this.battle.gen >= 4) return true;
+			if ('smackdown' in this.volatiles) return true;
+			const item = (this.ignoringItem() ? '' : this.item);
+			if (item === 'ironball') return true;
+			// If a Fire/Flying type uses Burn Up and Roost, it becomes ???/Flying-type, but it's still grounded.
+			if (!negateImmunity && this.hasType('Flying') && !('roost' in this.volatiles)) return false;
+			if (
+				(this.hasAbility('levitate') || this.hasAbility('magneticwaves')) &&
+				!this.battle.suppressingAttackEvents()
+			) return null;
+			if ('magnetrise' in this.volatiles) return false;
+			if ('telekinesis' in this.volatiles) return false;
+			return item !== 'airballoon';
 		},
 	},
 };
