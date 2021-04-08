90ef4dcd9d6629caf956fb2754e41246a9bd310d
HematiteH2O <71267054+HematiteH2O@users.noreply.github.com>
2021-04-07 21:41:28 -0600

    Pet Mod of the Season: Add Fusion Evolution UU

--- data/mods/feuu/formats-data.ts
+++ data/mods/feuu/formats-data.ts
@@ -1,5 +1,12 @@
 const mons = [
-// one sec
+	"volquag", "toxalure", "kingtsar", "tanette", "slowton", "flaant", "umbat", "chomplim", "chomplimmega", "xotalion", "miemie", "dusking",
+	"jelliswine", "pigapult", "silvino", "silvinomega", "lycanserkerdusk", "tapulop", "tapulopmega", "dragontler", "eternabat", "grimmlurk", "manicunogalar",
+	"yaciancrowned", "cryogolem", "stoudrago", "dongoro", "slurpum", "grousle", "grousleprimal", "corveot", "corveotmega", "igglyzentacrowned", "arctresgalar",
+	"garborude", "noicity", "ferros", "landmaldotherian", "tentoxysdefense", "strikadosgalar", "hooporant", "silvinobug", "silvinobugmega", "silvinodark",
+	"silvinodarkmega", "silvinodragon", "silvinodragonmega", "silvinoelectric", "silvinoelectricmega", "silvinofairy", "silvinofairymega", "silvinofighting",
+	"silvinofightingmega", "silvinofire", "silvinofiremega", "silvinoflying", "silvinoflyingmega", "silvinoghost", "silvinoghostmega", "silvinograss",
+	"silvinograssmega", "silvinoground", "silvinogroundmega", "silvinoice", "silvinoicemega", "silvinopoison", "silvinopoisonmega", "silvinopsychic",
+	"silvinopsychicmega", "silvinorock", "silvinorockmega", "silvinosteel", "silvinosteelmega", "silvinowater", "silvinowatermega",
 ];
 
 const data: {[k: string]: ModdedSpeciesFormatsData} = {};
